874a622178b2ef916b7c68d298d73c470153806d
mseben <mseben@gmail.com>
2022-12-10 09:17:13 +0100

    Update max7219-yfinance.py
    Generated by chat.openai.com

--- max7219-yfinance.py
+++ max7219-yfinance.py
@@ -1,31 +1,48 @@
-import yfinance as yf
-import time
+# Import the required modules
+import configparser
+import yahoo_fin.stock_info as yf
 import serial
-import math
 
+# Open the ttyUSB serial connection
 ttyUSB = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)
-time.sleep(2)
 
-sleep_time = 120
+# Create a ConfigParser object
+config = configparser.ConfigParser()
 
+# Read the ini file
+config.read('stock.ini')
+
+# Get the list of ticker symbols from the ini file
+tickers = config['DEFAULT']['stock_name'].split(',')
+
+# Get the frequency value from the ini file
+n = int(config['DEFAULT']['frequency'])
+
+# Loop forever
 while True:
-    stock = yf.Ticker("IWDA.AS")
-    price = stock.info['regularMarketPrice']
-    str_price = str(f'{price:.2f}') + "  iwda.as"
-    print(str_price)
-    ttyUSB.write(str_price.encode())
-    time.sleep(sleep_time)
-    
-    stock = yf.Ticker("ETH-EUR")
-    price = stock.info['regularMarketPrice']
-    str_price = str(f'{price:.2f}') + "  eth/eur"
-    print(str_price)
-    ttyUSB.write(str_price.encode())
-    time.sleep(sleep_time)
-    
-    stock = yf.Ticker("BTC-EUR")
-    price = stock.info['regularMarketPrice']
-    str_price = str(f'{price:.2f}') + "  btc/eur"
-    print(str_price)
-    ttyUSB.write(str_price.encode())
-    time.sleep(sleep_time)
+    # Loop through the list of ticker symbols
+    for ticker in tickers:
+        # Get the Ticker object for the stock with the ticker symbol
+        stock = yf.Ticker(ticker)
+
+        # Print the price every minute
+        print(stock.info['regularMarketPrice'])
+
+        # Get the price of the stock every n minutes
+        if i % n == 0:
+            price = stock.info['regularMarketPrice']
+
+            # Format the price as a string with two decimal places
+            str_price = str(f'{price:.2f}')
+
+            # Print the price
+            print(str_price)
+
+            # Write the price to the ttyUSB device
+            ttyUSB.write(str_price.encode())
+
+        # Increment the counter
+        i += 1
+
+    # Wait for 1 minute
+    time.sleep(60)
