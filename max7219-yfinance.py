3f9ce1b733fafb27e3c6eb57ed33bbad77b28fa6
mseben <mseben@gmail.com>
2023-01-22 10:17:56 +0100

    Get the latest data if stock market closed

--- max7219-yfinance.py
+++ max7219-yfinance.py
@@ -5,56 +5,56 @@ import random
 import threading
 from time import sleep
 
-def getStockPrices(stockName):
-    global price
-    stock = yf.Ticker(stockName)
-    price = stock.info['regularMarketPrice']
-#    price = random.randint(2,9)
-    print("[thread] price of ",stockName, " is ", price)
-    return price
+def getStockPrice(symbol):
+    ticker = yf.Ticker(symbol)
+    price = ticker.info['regularMarketPrice']
+    if price is None:
+        #todo try to use local cache
+        todaysData = ticker.history(period='1d')
+        return todaysData['Close'][0]
+    else:
+        return price
 
 def getStockPricesPeriodically(stockNames,getDataSec):
     global prices
     while True:
         for stockName in stockNames:
-            price = getStockPrices(stockName)
+            price = getStockPrice(stockName)
+            print("[thread] fetched price for", stockName, ":" , price)
             prices[stockName] = price
-        print("[thread] sleep for ", getDataSec)
+        print("[thread] sleep for", getDataSec)
         sleep(getDataSec)
 
 prices = {}
 config = configparser.ConfigParser()
-config.read('stock.ini')
-stockNames = config['yfinance']['stock_name'].split(',')
+config.read('max7219-yfinance.ini')
+stockNames = config['yfinance']['stock_names'].split(',')
 getDataSec = int(config['yfinance']['get_data_sec'])
 
 print(type(stockNames))
 t1 = threading.Thread(target=getStockPricesPeriodically,args=(stockNames,getDataSec))
 t1.start()
 
+refreshDisplaySec = int(config['max7219']['refresh_display_sec'])
+ttyDevice = config['max7219']['tty_device']
+print(ttyDevice)
+
+try:
+    ttyUSB = serial.Serial(ttyDevice, 9600, timeout=0.5)
+except:
+    print("Can't open",ttyDevice,"for write")
+    ttyUSB = None
+
+#todo: wait for fully set prices 
 sleep(10)
 
-refreshDisplaySec = int(config['max7219']['refresh_display_sec'])
 while True:
     for stockName in stockNames:
-        print("price of ",stockName," = ", prices[stockName])
-        print("sleep for ", refreshDisplaySec)
+        price = prices[stockName]
+        strPrice = str(f'{price:.2f}')
+        displayStr = strPrice + ' ' + stockName
+        print("show:",displayStr)
+        if ttyUSB != None:
+            ttyUSB.write(displayStr.encode())
+        print("sleep for", refreshDisplaySec)
         sleep(refreshDisplaySec)
-
-#ttyUSB = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)
-#config = configparser.ConfigParser()
-#config.read('stock.ini')
-#tickers = config['DEFAULT']['stock_name'].split(',')
-#n = int(config['DEFAULT']['frequency'])
-
-#while True:
-#    for ticker in tickers:
-#        stock = yf.Ticker(ticker)
-#        print(stock.info['regularMarketPrice'])
-#        if i % n == 0:
-#            price = stock.info['regularMarketPrice']
-#            str_price = str(f'{price:.2f}')
-#            print(str_price)
-#            ttyUSB.write(str_price.encode())
-#        i += 1
-#    time.sleep(60)
